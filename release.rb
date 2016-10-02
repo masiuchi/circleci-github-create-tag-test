fc027b08c13aa57968575df603d8d454b09899fc
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-10-02 14:07:14 +0900

    Fix

--- release.rb
+++ release.rb
@@ -2,8 +2,9 @@ require 'octokit'
 
 repo = 'masiuchi/circleci-github-create-tag-test'
 tag_name = `cat ./version`
+tag_name.chomp!
 
 client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
 
-client.create_release(repo, tag_name)
+release = client.create_release(repo, tag_name)
 client.upload_asset(release.url, './test.zip')
